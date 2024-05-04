39b7deb87abb925333f2ab182deef9865740b2a7
vickyrock008 <115275288+vickyrock008@users.noreply.github.com>
2024-05-04 15:12:43 +0530

    Time: 16 ms (97.17%), Space: 55.1 MB (17.06%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boats=0;
+        int light=0;
+        int heavy=people.length-1;;
+        while(light<=heavy)
+        {
+            if(people[light]+people[heavy]<=limit)
+            {
+                light++;
+            }
+            heavy--;
+            boats++;
+        }
+        return boats;
+    }
+}
