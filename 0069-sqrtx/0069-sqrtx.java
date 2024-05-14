ec52e4d8ad86d3cbfbc35648db0076808925cd19
vickyrock008 <115275288+vickyrock008@users.noreply.github.com>
2024-05-14 09:07:36 +0530

    Time: 1 ms (86.20%), Space: 40.5 MB (75.72%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,27 +1,29 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0 || x==1)
+    if(x==0 || x==1)
+    {
+        return x;
+    }
+    int start = 0;
+    int end = x;
+    int mid=-1;
+        
+    while(start<=end)
+    {
+         mid = start + (end-start)/2;
+        if((long)mid*mid==x)
         {
-            return x;
+            return mid;
         }
-        int start = 1;
-        int end = x;
-        while(start<=end)
+        else if((long)mid*mid>(long)x)
         {
-            int mid = start + (end-start)/2;
-            if((long)mid*mid>(long)x)
-            {
-                end = mid-1;
-            }
-            else if (mid*mid == x)
-            {
-                return mid;
-            }
-            else
-            {
-                start = mid+1;
-            }
+            end = mid-1;
         }
+        else
+        {
+             start=mid+1;   
+        }
+    }
         return Math.round(end);
     }
 }
