5dfd290a6d5c21ca7479e22bdc09a408c415c71f
vickyrock008 <115275288+vickyrock008@users.noreply.github.com>
2024-09-02 14:33:04 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (67.91%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+        String str = s.trim();
+        for(int i=str.length()-1;i>=0;i--)
+        {
+
+            if(str.charAt(i)==' ')
+            {
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
