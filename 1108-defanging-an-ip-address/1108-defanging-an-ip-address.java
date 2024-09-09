283f7e73bbf25ac7fa7745150f5c05a8426f9a9a
vickyrock008 <115275288+vickyrock008@users.noreply.github.com>
2024-09-09 15:05:17 +0530

    Time: 5 ms (26.09%), Space: 41.9 MB (5.26%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String res="";
+        for(int i=0;i<address.length();i++)
+        {
+            if(address.charAt(i)=='.')
+            {
+                res+='[';
+                res+=address.charAt(i);
+                res+=']';
+            }
+            else
+            {
+                 res+=address.charAt(i);
+            }
+           
+            
+        }
+        return res;
+    }
+}
